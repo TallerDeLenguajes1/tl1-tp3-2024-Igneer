fb380e218602d3ed8d9a8fa68d45e7fbaf4fba93
Pedro Salinas <pedroisalinas204@gmail.com>
2024-04-16 12:08:19 -0300

    Cargo mi librería y realizo el punto 2a)

---
+++ tp3_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "misFunciones.h"
+#define M = 5
+#define N = 12
+
+
+int main()
+{
+    
+    srand(time(NULL)); 
+
+    int produccion[M][N];
+
+    //a)
+    for(int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            produccion[i][j] = generarNumAleatorio(10000, 50000);
+        }
+    }
+    
+
+    return 0;
+}
