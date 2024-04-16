3c5f7157871ac57f456530c93d26772dd37d3834
Pedro Salinas <pedroisalinas204@gmail.com>
2024-04-16 13:30:01 -0300

    Resuelvo 2d)

--- tp3_2.c
+++ tp3_2.c
@@ -7,8 +7,7 @@
 
 
 int main()
-{
-    
+{   
     srand(time(NULL)); 
 
     int produccion[ANIOS][MESES], i, j;
@@ -34,18 +33,40 @@ int main()
     }
 
     //c)
-    float promedio = 0;
+    // float promedio = 0;
+    // for(i = 0; i < ANIOS; i++)
+    // {
+    //     for (j = 0; j < MESES; j++)
+    //     {
+    //         promedio += produccion[i][j];
+    //     }
+    //     promedio = (float)promedio/12; 
+    //     printf("Promedio de ventas del anio %d: %.2f\n", (i+1), promedio);
+    // }
+    
+    //d
+    int mayor = produccion[0][0], menor = produccion[0][0], anioMax = 0, mesMax = 0, anioMin = 0, mesMin = 0;
 
     for(i = 0; i < ANIOS; i++)
+    {
+        for(j = 0; j < MESES; j++)
         {
-            for (j = 0; j < MESES; j++)
+            if(mayor<produccion[i][j])
             {
-                promedio += produccion[i][j];
+                anioMax = i+1;
+                mesMax = j+1;
+                mayor = produccion[i][j];
             }
-            promedio = (float)promedio/12; 
-            printf("Promedio de ventas del anio %d: %.2f\n", (i+1), promedio);
+            if(menor>produccion[i][j])
+            {
+                anioMin = i+1;
+                mesMin = j+1;
+                menor = produccion[i][j];
+            }
         }
-    
+    }
+    printf("Mayor venta: %d Anio: %d Mes: %d \n", mayor, anioMax, mesMax);
+    printf("Menor venta: %d Anio: %d Mes: %d \n", menor, anioMin, mesMin);
 
     return 0;
 }
