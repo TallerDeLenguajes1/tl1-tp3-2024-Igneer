dd12d784fd374340001d7fb454da3d58f4796ded
Pedro Salinas <pedroisalinas204@gmail.com>
2024-04-16 12:09:22 -0300

    Resuelvo 2b)

--- tp3_2.c
+++ tp3_2.c
@@ -21,6 +21,16 @@ int main()
             produccion[i][j] = generarNumAleatorio(10000, 50000);
         }
     }
+
+    //b)
+    for(int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%d ", produccion[i][j]);
+        }
+        printf("\n");
+    }
     
 
     return 0;
